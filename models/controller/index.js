e3c9a1a52bd1f2b29f4da3166a94c5c1e85b28c1
segciest <minhduy1705@gmail.com>
2024-03-16 16:09:20 +0700

    8th

--- models/controller/index.js
+++ models/controller/index.js
@@ -187,16 +187,21 @@ render();
 const render2 = (arr = list.listPerson) => {
   //   console.log("hehe");
   //   let content = "";
+
   arr.forEach((item) => {
     let newUser = new Person();
     newUser = { ...newUser, ...item };
     const btn = `btn-${item.id}`;
-    switch (newUser.oop) {
+    switch (newUser.select) {
       case "student":
         const { toan, ly, hoa } = newUser;
+        console.log("tui học sinh");
         document
           .getElementById(btn)
-          .setAttribute("data-bs-title", `Toán:${toan} Lý:${ly} Hoá:${hoa}`);
+          .setAttribute(
+            "data-bs-title",
+            `Toán:${toan}<br> Lý:${ly}<br> Hoá:${hoa}`
+          );
         break;
       case "employee":
         const { soNgay, luongNgay } = newUser;
@@ -205,7 +210,7 @@ const render2 = (arr = list.listPerson) => {
           .getElementById(btn)
           .setAttribute(
             "data-bs-title",
-            `Lương ngày:${luongNgay} Số ngày:${soNgay}`
+            `Lương ngày:${luongNgay}<br> Số ngày:${soNgay}`
           );
         break;
       case "customer":
@@ -214,22 +219,24 @@ const render2 = (arr = list.listPerson) => {
           .getElementById(btn)
           .setAttribute(
             "data-bs-title",
-            `Công Ty: ${congTy} Hoá đơn: ${hoaDon} Đánh giá: ${rate}`
+            `Công Ty: ${congTy}<br> Hoá đơn: ${hoaDon}<br>  Đánh giá: ${rate}`
           );
         break;
     }
     // Khởi tạo Popover cho các nút "More"
-    arr.forEach((item) => {
-      const id = item.id;
-      const btn = document.getElementById(`btn-${id}`);
-      new bootstrap.Popover(btn, {
-        container: "body",
-      });
+  });
+  arr.forEach((item) => {
+    const id = item.id;
+    const btn = document.getElementById(`btn-${id}`);
+    new bootstrap.Popover(btn, {
+      container: "body",
+      html: true,
     });
   });
 };
 render2();
 
+// hàm xoá người dúng
 let deleteUser = (id) => {
   let index = list.listPerson.findIndex((item, index) => {
     return item.id == id;
@@ -242,6 +249,7 @@ let deleteUser = (id) => {
   }
 };
 
+// hàm cập nhật người dùng
 let updateUser = () => {
   const input = document.querySelectorAll(
     "#formUser input,#formUser select,#formUser textarea"
@@ -277,6 +285,7 @@ let updateUser = () => {
   render2();
 };
 
+// lấy dữ liệu ng dùng khi muốn chỉnh sửa
 let getDetailUser = (id) => {
   console.log("hehe");
 
@@ -318,6 +327,8 @@ let getDetailUser = (id) => {
     document.getElementById("id").readOnly = true;
   }
 };
+
+// sort theo id
 let sortId = () => {
   console.log("alibaba");
   let arr = [...list.listPerson];
@@ -326,6 +337,8 @@ let sortId = () => {
   render(arr);
   render2(arr);
 };
+
+// sort theo họ tên
 let sortHoTen = () => {
   list.listPerson.forEach((item, index) => {
     let a = { ...item };
@@ -348,7 +361,54 @@ let sortHoTen = () => {
   render(arr);
   render2(arr);
 };
+let searchUser = (event) => {
+  let input = event.target.value;
+  var keyword = input.trim().toLowerCase();
+  var newKeyword = removeVietnameseTones(keyword);
+  let arrFilter = [];
+  list.listPerson.forEach((item, index) => {
+    let user = item;
+    let data = removeVietnameseTones(user.select.trim().toLowerCase());
+    if (data.includes(newKeyword)) {
+      arrFilter.push(user);
+    }
+  });
+  render(arrFilter);
+  render2(arrFilter);
+};
 
+// Hàm bỏ dấu tiếng việt
+function removeVietnameseTones(str) {
+  str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a");
+  str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
+  str = str.replace(/ì|í|ị|ỉ|ĩ/g, "i");
+  str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, "o");
+  str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, "u");
+  str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, "y");
+  str = str.replace(/đ/g, "d");
+  str = str.replace(/À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ/g, "A");
+  str = str.replace(/È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ/g, "E");
+  str = str.replace(/Ì|Í|Ị|Ỉ|Ĩ/g, "I");
+  str = str.replace(/Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ/g, "O");
+  str = str.replace(/Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ/g, "U");
+  str = str.replace(/Ỳ|Ý|Ỵ|Ỷ|Ỹ/g, "Y");
+  str = str.replace(/Đ/g, "D");
+  // Some system encode vietnamese combining accent as individual utf-8 characters
+  // Một vài bộ encode coi các dấu mũ, dấu chữ như một kí tự riêng biệt nên thêm hai dòng này
+  str = str.replace(/\u0300|\u0301|\u0303|\u0309|\u0323/g, ""); // ̀ ́ ̃ ̉ ̣  huyền, sắc, ngã, hỏi, nặng
+  str = str.replace(/\u02C6|\u0306|\u031B/g, ""); // ˆ ̆ ̛  Â, Ê, Ă, Ơ, Ư
+  // Remove extra spaces
+  // Bỏ các khoảng trắng liền nhau
+  str = str.replace(/ + /g, " ");
+  str = str.trim();
+  // Remove punctuations
+  // Bỏ dấu câu, kí tự đặc biệt
+  str = str.replace(
+    /!|@|%|\^|\*|\(|\)|\+|\=|\<|\>|\?|\/|,|\.|\:|\;|\'|\"|\&|\#|\[|\]|~|\$|_|`|-|{|}|\||\\/g,
+    " "
+  );
+  return str;
+}
 window.onload = () => {
   window.deleteUser = (id) => {
     console.log(id);
@@ -366,4 +426,13 @@ window.onload = () => {
   window.sortId = () => {
     sortId();
   };
+  window.searchUser = (event) => {
+    searchUser(event);
+  };
+  //   window.removeVietnameseTones = (str) => {
+  //     removeVietnameseTones(str);
+  //   };
+  //   window.render2 = () => {
+  //     render2();
+  //   };
 };
